df5a23b61f458581a160130d3bc803e0df7f55bc
vanloi123 <33539252+vanloi123@users.noreply.github.com>
2026-08-14 09:16:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Xem hướng dẫn lấy 2 giá trị này trong file huong-dan-trien-khai.md
 // ============================================================
 
-export const SUPABASE_URL = "https://eukkftecjmxpvzzziejc.supabase.co";
+export const SUPABASE_URL = "https://eukkftecjmxpvzzziejc.supabase.co/rest/v1/";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1a2tmdGVjam14cHZ6enppZWpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NTU4NTcsImV4cCI6MjEwMjIzMTg1N30.lkrOMtAv-X2VD-_OJzxDBeK6P9iHnKRLbK27LdNUbbQ";
