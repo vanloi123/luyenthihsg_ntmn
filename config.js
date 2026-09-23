f37433f2f029dd947523fc77bea6e41ef63393dc
vanloi123 <33539252+vanloi123@users.noreply.github.com>
2026-09-23 14:22:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiO
 
 
 export const SISTER_SITE_URL = "https://vanloi123.info.vn/";
-export const SISTER_SITE_LABEL = "Đội tuyển Tin học 10";
+export const SISTER_SITE_LABEL = "Sang đội tuyển Tin học 10";
